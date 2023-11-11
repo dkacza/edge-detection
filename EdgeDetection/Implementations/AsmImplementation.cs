eb3983b9ac752643a7fb2696565bd70c83f6d7fd
Dawid Kacza <dawidkacza02@gmail.com>
2023-11-11 16:58:19 +0100

    Added initial ASM file

--- EdgeDetection/Implementations/AsmImplementation.cs
+++ EdgeDetection/Implementations/AsmImplementation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -9,10 +10,17 @@ namespace EdgeDetection.Implementations
 {
     internal class AsmImplementation : IConverter
     {
+        [DllImport(@"D:\Dev\CS\edge-detection\x64\Debug\AsmLibrary.dll")]
+        static extern int MyProc1(int a, int b);
         public long Convert(string inputPath, string outputPath, int cores, int threshold)
         {
-            String diagnosticMsg = $"Converting with x86 assembly.\nInput path: {inputPath}\nOutput path: {outputPath}\nThreshold: {threshold}\nCores: {cores}";
-            MessageBox.Show(diagnosticMsg);
+
+            int x = 5, y = 3;
+            int retVal = MyProc1(x, y);
+            string message = "x86 assembly result:" + retVal;
+
+
+            MessageBox.Show(message);
             return 0L;
         }
     }
